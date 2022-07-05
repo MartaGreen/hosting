06ac3611faac58ef5b6968811de6cb124bc7144d
MartaGreen <iguana13131@gmail.com>
2022-07-05 18:18:21 +0400

    feat: add normalize

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: plugins,
