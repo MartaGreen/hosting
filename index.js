dffd9cbf14ce802b9c573f34d28b54007139852f
MartaGreen <iguana13131@gmail.com>
2022-07-05 19:57:17 +0400

    feat: add header

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 import "normalize.css";
 
 import "./src/index.scss";
+import "./src/header/header.scss";
 
 // files
-import "./assets/header/header_logo.svg";
+import "./assets/images/header/header_logo.svg";
+import "./assets/images/header/number_icon.svg";
+import "./assets/images/header/mail_icon.svg";
+import "./assets/images/header/user_icon.svg";
