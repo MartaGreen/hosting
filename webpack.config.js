db2aa16a10a6c886bc00c441cd4923a93c43b925
MartaGreen <iguana13131@gmail.com>
2022-07-05 17:51:02 +0400

    init: add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+const path = require("path");
+
+const plugins = [
+  new HTMLWebpackPlugin({
+    title: "Hosting",
+    filename: "index.html",
+    template: path.join(__dirname, "src/index.pug"),
+  }),
+  new HtmlWebpackPugPlugin(),
+  new CleanWebpackPlugin(),
+];
+
+module.exports = {
+  entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: "pug-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: plugins,
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    onListening: (devServer) => {
+      if (!devServer) {
+        throw new Error("webpack-dev-server is not defined");
+      }
+
+      const port = devServer.server.address().port;
+      console.log(`Listening on port:, ${port}`);
+    },
+    open: true,
+  },
+};
