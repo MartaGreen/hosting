66e17c3c42369bd3a7fbaf04480221c4ca30c49a
MartaGreen <iguana13131@gmail.com>
2022-07-06 16:53:08 +0400

    feat: add hosting btn

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ import "./assets/images/header/header_logo.svg";
 import "./assets/images/header/number_icon.svg";
 import "./assets/images/header/mail_icon.svg";
 import "./assets/images/header/user_icon.svg";
+
+import "./assets/images/main-info/arrow.svg";
