e0bdf3ec0ee0e90cfd219aa68a388435b9f3a304
MartaGreen <iguana13131@gmail.com>
2022-07-08 17:14:57 +0400

    feat: add tariffs cards

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import "normalize.css";
 import "./src/index.scss";
 import "./src/header/header.scss";
 import "./src/main-info/main-info.scss";
+import "./src/tariffs/tariffs.scss";
+import "./src/tariffs/card/card.scss";
 
 // files
 import "./assets/images/header/header_logo.svg";
@@ -12,3 +14,8 @@ import "./assets/images/header/mail_icon.svg";
 import "./assets/images/header/user_icon.svg";
 
 import "./assets/images/main-info/arrow.svg";
+
+import "./assets/images/tariffs/bisnes.png";
+import "./assets/images/tariffs/mini.png";
+import "./assets/images/tariffs/standart.png";
+import "./assets/images/tariffs/start.png";
